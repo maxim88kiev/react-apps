534e8663e73097af50dd6051756f613dfd643e45
Maxim <maxim88kiev@gmail.com>
2019-06-05 16:56:33 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@
 
 const getResource = async (url) => {
     const res = await fetch(url);
+    if(!res.ok){
+        throw new Error(`Could not fetch ${url}` + `, received ${res.status}`)
+    }
     const body = await res.json();
     return body;
 };
 
 
-
-getResource('https://swapi.co/api/people/1/')
+getResource('https://swapi.co/api/people/143535/')
     .then((body) => {
     console.log(body);
+}).catch((err) => {
+    console.error('Could not fetch', err);
 });
+
